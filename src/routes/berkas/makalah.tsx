acfce5820515464acce5d3ec7fa44ebcb97c0773
Naufal RF <naufalrf4@gmail.com>
2026-07-20 10:26:55 +0700

    chore: sync makalah final (judul kapital) [skip ci]

--- src/routes/berkas/makalah.tsx
+++ src/routes/berkas/makalah.tsx
@@ -4,5 +4,5 @@ import { PdfViewer } from "#/components/berkas/PdfViewer"
 export const Route = createFileRoute("/berkas/makalah")({ component: Makalah })
 
 function Makalah() {
-  return <PdfViewer src="/pdf/makalah.pdf?v=20260719b" title="Makalah" />
+  return <PdfViewer src="/pdf/makalah.pdf?v=20260720" title="Makalah" />
 }
